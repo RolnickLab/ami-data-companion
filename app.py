f91b51cd75d1356ed1ac8d1b95c4b9225668ace7
Michael Bunsen <notbot@gmail.com>
2022-07-14 16:39:50 -0700

    Improve example interface, formatting, icon

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
+#! /usr/bin/env python3
+
 import pathlib
 import random
 import tempfile
 import dateutil.parser
 import logging
+import requests
+import base64
+import io
 
 
 import kivy
@@ -14,11 +19,15 @@ from kivy.app import App
 from kivy.uix.label import Label
 from kivy.uix.image import Image
 from kivy.uix.button import Button
+from kivy.uix.popup import Popup
 from kivy.uix.widget import Widget
+from kivy.graphics import Rectangle, Color
 from kivy.uix.gridlayout import GridLayout
 from kivy.lang import Builder
+from kivy.properties import StringProperty, ListProperty, ObjectProperty
 
 from plyer import filechooser
+import PIL
 
 
 logger = logging.getLogger(__name__)
@@ -89,7 +98,8 @@ def find_timestamped_folders(path):
         # @TODO use yield?
         try:
             date = dateutil.parser.parse(_preprocess(d.name))
-        except dateutil.parser.ParserError:
+        except Exception:
+            # except dateutil.parser.ParserError:
             pass
         else:
             nights[date] = d
@@ -98,7 +108,111 @@ def find_timestamped_folders(path):
     return nights
 
 
+def find_images(path):
+    images = [
+        f for f in path.iterdir() if f.suffix.lower() in SUPPORTED_IMAGE_EXTENSIONS
+    ]
+    return images
+
+
+def predict_image(path):
+    img = PIL.Image.open(path)
+    buffered = io.BytesIO()
+    img.save(buffered, format="JPEG")
+    img_str = base64.b64encode(buffered.getvalue())
+    resp = requests.post("http://localhost:5000/predict", data={"b64": img_str})
+    resp.raise_for_status()
+    results = resp.json()
+    return results
+
+
+class CanvasWidget(Widget):
+    def __init__(self, **kwargs):
+
+        super(CanvasWidget, self).__init__(**kwargs)
+        image_path = kwargs.pop("image_path")
+
+        # Arranging Canvas
+        with self.canvas:
+
+            Color(0.234, 0.456, 0.678, 0.8)  # set the colour
+
+            # Setting the size and position of canvas
+            self.rect = Rectangle(
+                pos=self.center, size=(self.width / 2.0, self.height / 2.0)
+            )
+            self.bg = Image(
+                source=str(image_path.absolute()), pos=self.pos, size=self.size
+            )
+
+            # Update the canvas as the screen size change
+            self.bind(pos=self.update_rect, size=self.update_rect)
+
+    # update function which makes the canvas adjustable.
+    def update_rect(self, *args):
+        self.rect.pos = self.pos
+        self.rect.size = self.size
+
+
+class TrapSesionData(Widget):
+    """
+    One night / session of trap data.
+
+    Will keep track of which directories have been processed, their cached results, etc.
+    Could be backed by a SQLite database? Or just a folder structure under .cache
+    """
+
+    pass
+
+
+class AnalyzeButton(Button):
+    path = ObjectProperty()
+
+    def on_release(self):
+        self.analyze()
+
+    def analyze(self):
+        """
+        Process an image directory.
+
+        For now, just classify a random image.
+        """
+        images = find_images(self.path)
+        img_path = random.choice(images)
+        results = predict_image(img_path)
+        for widget in self.parent.children:
+            # @TODO should we register nightly folders by ID somewhere?
+            if isinstance(widget, Button):
+                widget.disabled = False
+        self.show_results(img_path, results)
+
+    def show_results(self, img_path, results):
+        content = GridLayout(rows=3, cols=1, spacing=20)
+        content.add_widget(Image(source=str(img_path)))
+
+        result_text = Label(
+            text=f"{results['highest_confidence_label']} \n{results['highest_confidence']}"
+        )
+
+        content.add_widget(result_text)
+
+        close_button = Button(text="Close", size=(100, 20))
+        # content.add_widget(close_button)
+
+        popup = Popup(
+            title="Classification results",
+            content=content,
+            auto_dismiss=True,
+            size_hint=(None, None),
+            size=(400, 400),
+        )
+        close_button.bind(on_press=popup.dismiss)
+        popup.open()
+
+
 class MainLayout(Widget):
+    root_dir = ObjectProperty()
+
     def choose_root_directory(self):
         # @TODO "$HOME" doesn't work, maybe choose SDCARD / USB Device first?
         # /home/michael/Projects/AMI/data/TrapData_2022
@@ -107,44 +221,53 @@ class MainLayout(Widget):
             "last_root_dir",
             filechooser.choose_dir(
                 title="Choose the root directory for your nightly trap data",
-                path="$HOME",
+                path="/media/michael/LaCie/AMI/",
             )[0],
         )
 
+        self.root_dir = root_dir
+
+        # Initialize trap data objects
         nightly_folders = find_timestamped_folders(root_dir)
-        self.display_folders(nightly_folders)
+
+        return nightly_folders
 
     def display_folders(self, folders):
-
         grid = self.ids.nightly_folders
 
         grid.children = []
         for date, path in folders.items():
-            images = [
-                f for f in path.iterdir() if f.suffix.lower() in SUPPORTED_IMAGE_EXTENSIONS
-            ]
+
+            images = find_images(path)
+
+            label = f"{date.strftime('%a, %b %-d')} \n{len(images)} images"
             bg_image = str(random.choice(images).absolute())
-            label = f"{date.strftime('%c')} \n({len(images)} images)"
-            button = Button(
-                text=label,
-                # color=(0, 0, 1, 1),
-                # color=(0, 0, 0, 1),
-                # bold=True,
-                font_size="24sp",
-                # outline_color=(1, 1, 1),
-                # outline_width=1,
-                # background_normal=bg_image,
-                # border = (30, 30, 30, 30),
-            )
-            callback = lambda btn: print(path, btn)
-            button.bind(on_release=callback)
-            grid.add_widget(button)
+
+            analyze_btn = AnalyzeButton(text="Analyze", path=path)
+            # analyze_btn.bind(on_release=self.analyze_callback)
+
+            find_species_btn = Button(text="List species", disabled=True)
+            play_video_btn = Button(text="Play video", disabled=True)
+
+            row = GridLayout(rows=1, cols=5, spacing=10)
+            row.add_widget(Image(source=bg_image))
+            row.add_widget(Label(text=label))
+            row.add_widget(analyze_btn)
+            row.add_widget(find_species_btn)
+            row.add_widget(play_video_btn)
+            grid.add_widget(row)
+
+    def draw_image(self, path):
+        grid = self.ids.nightly_folders
+        grid.children = []
+        grid.add_widget(CanvasWidget(image_path=path))
 
 
 class TrapDataAnalyzer(App):
     def build(self):
         layout = MainLayout()
-        layout.choose_root_directory()
+        nightly_folders = layout.choose_root_directory()
+        layout.display_folders(nightly_folders)
         return layout
 
 
