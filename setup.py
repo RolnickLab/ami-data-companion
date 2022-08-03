bac5af8f1c22ace7416e2cce320128ac5122280a
Michael Bunsen <notbot@gmail.com>
2022-08-02 19:17:28 -0700

    Add setup.py for packaging

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+
+def readme():
+    with open("README.md") as f:
+        return f.read()
+
+
+setup(
+    name="trapdata",
+    version="0.1",
+    description="Deskop app for viewing and procesing data from autonomous insect monitoring stations.",
+    url="http://github.com/notbot/trapdata",
+    author="Michael Bunsen",
+    author_email="notbot@gmail.com",
+    license="MIT",
+    packages=["trapdata"],
+    zip_safe=False,
+    entry_points={"console_scripts": ["trapdata=trapdata.main:main"]},
+)
