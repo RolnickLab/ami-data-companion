0939a43d16d2e0297cf6da359a50b30d2fd02c21
Michael Bunsen <notbot@gmail.com>
2022-08-09 17:09:28 -0700

    Example of a background task with progress

--- trapdata/menu.py
+++ trapdata/menu.py
@@ -1,14 +1,18 @@
 #! /usr/bin/env python3
 
+import asyncio
+import time
 import pathlib
 import random
 import tempfile
+from xmlrpc.client import Boolean
 import dateutil.parser
 import logging
 import requests
 import base64
 import io
 from functools import partial
+from threading import Thread
 
 import kivy
 
@@ -26,7 +30,13 @@ from kivy.graphics import Rectangle, Color, Canvas, Line
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.relativelayout import RelativeLayout
 from kivy.lang import Builder
-from kivy.properties import StringProperty, ListProperty, ObjectProperty
+from kivy.properties import (
+    StringProperty,
+    ListProperty,
+    ObjectProperty,
+    NumericProperty,
+    BooleanProperty,
+)
 from kivy.uix.screenmanager import Screen
 from kivy.logger import Logger
 
@@ -36,6 +46,18 @@ from .utils import *
 from .ml import detect_and_classify
 
 # detect_and_classify = lambda *args, **kwargs: None
+
+
+async def say_after(delay, what):
+    await asyncio.sleep(delay)
+    print(what)
+
+
+async def process_async_test(status):
+    print(f"started at {time.strftime('%X')}")
+    await say_after(1, "hello, one second")
+    await say_after(2, "world, two seconds")
+    print(f"completed at {time.strftime('%X')}")
 
 
 Builder.load_file(str(pathlib.Path(__file__).parent / "menu.kv"))
@@ -54,54 +76,83 @@ class TrapSesionData(Widget):
 
 class AnalyzeButton(Button):
     path = ObjectProperty()
+    running = BooleanProperty(defaultvalue=False)
+    progress = NumericProperty(defaultvalue=0)
+    status_label = ObjectProperty()
+    progress_label = ObjectProperty()
+    popup = ObjectProperty()
+    bgtask = ObjectProperty()
 
     def on_release(self):
-        self.analyze()
+        if not self.popup:
+            self.make_popup()
+        self.popup.open()
+
+    def start(self, *args):
+        # @TODO can use the status property of the bgtask thread instead
+        if not self.running:
+            self.running = True
+            self.progress = 0
+            self.bgtask = Thread(target=self.analyze)
+            self.bgtask.start()
+
+    def stop(self, *args):
+        raise NotImplementedError
+        if self.running:
+            self.running = False
+            if self.bgtask:
+                self.bgtask.stop()
 
     def analyze(self):
-        """
-        Process an image directory.
+        while self.progress < 20:
+            time.sleep(0.5)
+            self.progress += 1
+        self.running = False
 
-        For now, just classify a random image.
-        """
-        annotations = detect_and_classify(self.path)
+        # annotations = detect_and_classify(self.path)
         # images = find_images(self.path)
         # img_path = random.choice(images)
         # results = predict_image(img_path)
+        # self.show_results(img_path, results)
+
+    def complete(self):
         for widget in self.parent.children:
             # @TODO should we register nightly folders by ID somewhere?
             if isinstance(widget, Button):
                 widget.disabled = False
-        self.disabled = True
-        # self.show_results(img_path, results)
+        self.disabled = False
 
-    def show_results(self, img_path, results):
-        content = GridLayout(rows=3, cols=1, spacing=20)
-        img_container = RelativeLayout()
-        img = Image(source=str(img_path), keep_ratio=True, allow_stretch=True)
-        rect = Line(rounded_rectangle=(0, 0, 100, 100, 5))
-        img.canvas.add(rect)
-        img_container.add_widget(img)
-        content.add_widget(img_container)
+    def make_popup(self):
+        if not self.popup:
+            content = GridLayout(rows=5, cols=1, spacing=5)
+            self.status_label = Label(text=f"Running: {self.running}")  # , id="status")
+            self.progress_label = Label(
+                text=f"Progress: {self.progress}"  # , id="progress"
+            )
+            content.add_widget(self.status_label)
+            content.add_widget(self.progress_label)
+            close_button = Button(text="Close progress", size=(100, 20))
+            start_button = Button(text="Start", size=(100, 20))
+            start_button.bind(on_press=self.start)
+            stop_button = Button(text="Stop", size=(100, 20))
+            stop_button.bind(on_press=self.stop)
+            content.add_widget(start_button)
+            content.add_widget(stop_button)
+            content.add_widget(close_button)
+            self.popup = Popup(
+                title="Status",
+                content=content,
+                auto_dismiss=True,
+                size_hint=(None, None),
+                size=(400, 400),
+            )
+            close_button.bind(on_press=self.popup.dismiss)
 
-        result_text = Label(
-            text=f"{results['highest_confidence_label']} \n{results['highest_confidence']}"
-        )
+    def on_progress(self, instance, value):
+        self.progress_label.text = f"Progress: {value}"
 
-        content.add_widget(result_text)
-
-        close_button = Button(text="Close", size=(100, 20))
-        # content.add_widget(close_button)
-
-        popup = Popup(
-            title="Classification results",
-            content=content,
-            auto_dismiss=True,
-            size_hint=(None, None),
-            size=(400, 400),
-        )
-        close_button.bind(on_press=popup.dismiss)
-        popup.open()
+    def on_running(self, instance, value):
+        self.status_label.text = f"Running: {value}"
 
 
 class PlaybackButton(Button):
