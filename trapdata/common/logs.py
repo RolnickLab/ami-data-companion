afc41928e97f2d23ad07f4377104f7871238155b
Michael Bunsen <notbot@gmail.com>
2022-09-28 21:36:50 -0700

    Add formatting to logger

--- trapdata/common/logs.py
+++ trapdata/common/logs.py
@@ -1,9 +1,15 @@
 import logging
 import sys
 
-logger = logging.getLogger().getChild(__name__)
+logger = logging.getLogger(__name__)
+# logger = logging.getLogger().getChild(__name__)
+formatter = logging.Formatter(fmt="[%(levelname)s] %(message)s")
+handler = logging.StreamHandler()
+handler.setFormatter(formatter)
+
 logger.setLevel(logging.DEBUG)
-logger.addHandler(logging.StreamHandler())
+logger.addHandler(handler)
+
 
 # Use different config if running tests
 if hasattr(sys.modules["__main__"], "_SpoofOut"):
