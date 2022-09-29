b4b051042c2a391504c51ab6579f3c9b3d268071
Michael Bunsen <notbot@gmail.com>
2022-09-28 22:06:37 -0700

    Readable logs!

--- trapdata/common/logs.py
+++ trapdata/common/logs.py
@@ -1,17 +1,4 @@
-import logging
-import sys
-
-logger = logging.getLogger(__name__)
-# logger = logging.getLogger().getChild(__name__)
-formatter = logging.Formatter(fmt="[%(levelname)s] %(message)s")
-handler = logging.StreamHandler()
-handler.setFormatter(formatter)
-
-logger.setLevel(logging.DEBUG)
-logger.addHandler(handler)
+import structlog
 
 
-# Use different config if running tests
-if hasattr(sys.modules["__main__"], "_SpoofOut"):
-    logger.setLevel(logging.DEBUG)
-    logger.addHandler(logging.StreamHandler())
+logger = structlog.get_logger()
