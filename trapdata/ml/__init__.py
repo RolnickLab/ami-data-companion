457ec7581ce6097eaefdb72eb37e342e58a29342
Michael Bunsen <notbot@gmail.com>
2022-09-30 18:26:58 -0700

    Begin adding general model inference class

--- trapdata/ml/__init__.py
+++ trapdata/ml/__init__.py
@@ -1,5 +1,108 @@
-import glob
-import sys
+import torch
+import torchvision
+
+from trapdata import logger
+from trapdata.ml.utils import get_device, StopWatch
+
+
+class InferenceModel:
+    key = None
+    title = None
+    description = None
+    device = None
+    weights = None
+    labels = None
+    model = None
+    transforms = None
+    batch_size = 4
+    num_workers = 2
+    results_callback = None
+
+    def __init__(self, *args, **kwargs):
+        for k, v in kwargs:
+            setattr(self, k, v)
+
+        logger.info(
+            f"Initializing model {self.name} on device: {self.device} with weights: {self.weights}"
+        )
+
+        self.device = self.device or get_device()
+        self.model = self.get_model(self.weights)
+        self.transforms = self.get_transforms()
+        self.dataset = self.get_dataset()
+        self.dataloader = self.get_dataloader()
+
+    def get_model(self, weights):
+        model = torch.nn.Module()
+        model.load_state_dict(weights)
+        model = model.to(self.device)
+        model.eval()
+        return model
+
+    def get_transforms(self):
+        transforms = torchvision.transforms.Compose(
+            [
+                torchvision.transforms.ToTensor(),
+            ]
+        )
+        return transforms
+
+    def get_dataset(self):
+        dataset = torch.utils.data.Dataset(
+            image_transforms=self.get_transforms(),
+        )
+        return dataset
+
+    def get_dataloader(self):
+        logger.info(
+            f"Preparing dataloader with batch size of {self.batch_size} and {self.num_workers} workers."
+        )
+        self.dataloader = torch.utils.data.DataLoader(
+            self.dataset,
+            batch_size=self.batch_size,
+            num_workers=self.num_workers,
+            persistent_workers=True,
+            shuffle=False,
+            pin_memory=True,  # @TODO review this
+        )
+        return self.dataloader
+
+    def predict_batch(self, batch):
+        batch_input = batch.to(
+            self.device,
+            non_blocking=True,  # Block while in development, are we already in a background process?
+        )
+        batch_output = self.model(batch_input)
+        return batch_output
+
+    def format_output(self, batch_output):
+        return batch_output
+
+    def save_results(self, results):
+        logger.warn("No save method configured for model. Doing nothing with results")
+        return None
+
+    def run(self):
+        with torch.no_grad():
+            for i, batch_input in enumerate(self.dataloader):
+
+                logger.info(f"Running batch {i+1} out of {len(self.dataloader)}")
+
+                with StopWatch() as batch_time:
+                    batch_output = self.predict_batch(batch_input)
+
+                seconds_per_item = batch_time.duration / len(batch_output)
+
+                batch_output = self.format_output(batch_output)
+
+                logger.info(
+                    f"Inference time for batch: {batch_time}\n"
+                    f"{round(seconds_per_item, 1)} seconds per item"
+                )
+
+                self.save_results(batch_output)
+
+
 import importlib
 
 from trapdata import logger
@@ -59,21 +162,3 @@ def classify_objects(model_name, **kwargs):
     logger.debug(f"Calling predict with arguments: {kwargs}")
     model_module.predict(**kwargs)
     logger.debug("Predict complete")
-
-
-def watch_queue(db):
-    """
-    query db for new images to detect ojects in
-    create a batch, remove from initial queue.
-
-    query db for objects that need cropping & initial classification
-
-    query db that need fine-grained taxon classification
-    """
-    pass
-
-
-if __name__ == "__main__":
-    source_dir = sys.argv[1]
-    image_list = glob(source_dir)
-    detect_objects(source_dir, image_list)
