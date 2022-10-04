a14e1046471734d8f19440a1c7ab94e368c9b132
Michael Bunsen <notbot@gmail.com>
2022-10-03 21:50:30 -0700

    Use class based models from UI

--- trapdata/ml/models/__init__.py
+++ trapdata/ml/models/__init__.py
@@ -4,6 +4,8 @@ from .classification import (
     SpeciesClassifier,
 )
 
-object_detectors = ObjectDetector.__subclasses__()
-binary_classifiers = BinaryClassifier.__subclasses__()
-species_classifiers = SpeciesClassifier.__subclasses__()
+object_detectors = {Model.name: Model for Model in ObjectDetector.__subclasses__()}
+binary_classifiers = {Model.name: Model for Model in BinaryClassifier.__subclasses__()}
+species_classifiers = {
+    Model.name: Model for Model in SpeciesClassifier.__subclasses__()
+}
