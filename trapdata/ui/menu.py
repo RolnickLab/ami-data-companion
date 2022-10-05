47281e883b4048b96d1d04b65fe11ab3cea363f4
Michael Bunsen <notbot@gmail.com>
2022-10-04 20:57:11 -0700

    flake8 fixes

--- trapdata/ui/menu.py
+++ trapdata/ui/menu.py
@@ -1,5 +1,4 @@
 import pathlib
-import threading
 
 import kivy
 from kivy.app import App
@@ -8,7 +7,6 @@ from kivy.uix.label import Label
 from kivy.uix.image import AsyncImage
 from kivy.uix.button import Button
 from kivy.uix.popup import Popup
-from kivy.uix.widget import Widget
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.recycleview import RecycleView
