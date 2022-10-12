f364e0e593fd371c091afbacbe1b8330029b26c7
Michael Bunsen <notbot@gmail.com>
2022-10-11 23:44:25 -0700

    Reenable Sentry logging

--- trapdata/__init__.py
+++ trapdata/__init__.py
@@ -1,9 +1,9 @@
-# import sentry_sdk
-#
-# sentry_sdk.init(
-#     dsn="https://d2f65f945fe343669bbd3be5116d5922@o4503927026876416.ingest.sentry.io/4503927029497856",
-#     traces_sample_rate=1.0,
-# )
+import sentry_sdk
+
+sentry_sdk.init(
+    dsn="https://d2f65f945fe343669bbd3be5116d5922@o4503927026876416.ingest.sentry.io/4503927029497856",
+    traces_sample_rate=1.0,
+)
 
 import multiprocessing
 
