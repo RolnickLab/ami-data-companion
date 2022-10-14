47d9632492081cc08cdd80a53723004b77910efd
Michael Bunsen <notbot@gmail.com>
2022-10-14 16:53:47 -0700

    Reduce log level

--- trapdata/common/logs.py
+++ trapdata/common/logs.py
@@ -4,7 +4,7 @@ import structlog
 
 
 structlog.configure(
-    wrapper_class=structlog.make_filtering_bound_logger(logging.DEBUG),
+    wrapper_class=structlog.make_filtering_bound_logger(logging.INFO),
 )
 
 logger = structlog.get_logger()
