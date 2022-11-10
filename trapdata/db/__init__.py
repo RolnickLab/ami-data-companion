dcf13b7fc8cd2b0a39ba44dd573cd5419add266d
Michael Bunsen <notbot@gmail.com>
2022-11-09 21:01:06 -0800

    Start using new db sessions

--- trapdata/db/__init__.py
+++ trapdata/db/__init__.py
@@ -1,9 +1,9 @@
 import sqlalchemy as sa
 from sqlalchemy import orm
-from .base import get_db, check_db, get_session
+from .base import get_db, check_db, get_session, get_session_class
 
 
-__all__ = [sa, orm, get_db, check_db, get_session]
+__all__ = [sa, orm, get_db, check_db, get_session, get_session_class]
 
 # Only call this once & reuse it
 Base = orm.declarative_base()
