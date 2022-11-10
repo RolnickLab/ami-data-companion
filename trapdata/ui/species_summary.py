ccff94595ab9d610d0cd6ebcedfee98aba7db403
Michael Bunsen <notbot@gmail.com>
2022-11-09 21:00:28 -0800

    Attempt to show grid of moth thumbnails

--- trapdata/ui/species_summary.py
+++ trapdata/ui/species_summary.py
@@ -6,21 +6,27 @@ from kivy.uix.screenmanager import Screen
 from kivy.properties import ObjectProperty
 from kivy.uix.label import Label
 from kivy.uix.recycleview import RecycleView
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.stacklayout import StackLayout
 from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.popup import Popup
-from kivy.uix.image import Image
+from kivy.uix.image import Image, AsyncImage
 from kivy.lang import Builder
 from kivy.clock import Clock
 
 from trapdata import logger
-from trapdata.db import queries
-from trapdata.db.models.detections import get_detected_objects, export_detected_objects
+from trapdata.db.models.detections import (
+    get_unique_species,
+    get_objects_for_species,
+)
 
 
-Builder.load_file(str(pathlib.Path(__file__).parent / "summary.kv"))
+Builder.load_file(str(pathlib.Path(__file__).parent / "species_summary.kv"))
+
+from kivy.uix.recycleview.views import RecycleDataViewBehaviddor
 
 
-class SpeciesRow(BoxLayout):
+class SpeciesGrid(StackLayout):
     species = ObjectProperty()
 
     def __init__(self, **kwargs):
@@ -28,29 +34,46 @@ class SpeciesRow(BoxLayout):
 
     def on_species(self, instance, value):
         if self.species:
-            self.make_row(self.species)
+            app = App.get_running_app()
+            detections = get_objects_for_species(
+                app.db_path, self.species["name"], self.species["monitoring_session"]
+            )
+            logger.info(f"Showing all detections for species: {self.species['name']}")
+            self.make_row(self.species, detections)
 
-    def make_row(self, species):
+    def make_row(self, species, detections):
         self.clear_widgets()
         label = Label(
             text=species["name"],
             halign="left",
-            valign="middle",
+            valign="top",
+            size_hint_x=0.2,
         )
         label.bind(size=label.setter("text_size"))
 
-        self.add_widget(
-            Image(
-                source=species["image_path"],
-                size_hint_y=None,
-                height=species["image_height"],
-            )
+        # self.add_widget(label)
+
+        stack = StackLayout(
+            orientation="lr-tb",
+            spacing=(0, 0),
+            padding=(0, 0),
+            size_hint_x=0.8,
+            size_hint_y=None,
         )
-        self.add_widget(label)
-        self.add_widget(Label(text=str(species["count"]), valign="top"))
+
+        for row in detections[:5]:
+            obj = row[0]
+            self.add_widget(
+                Image(
+                    source=obj.path,
+                    width=100,
+                    height=100,
+                )
+            )
+        # self.add_widget(stack)
 
 
-class SpeciesListLayout(RecycleView):
+class SpeciesGridLayout(RecycleView):
     monitoring_session = ObjectProperty()
     refresh_clock = ObjectProperty(allownone=True)
 
@@ -60,7 +83,7 @@ class SpeciesListLayout(RecycleView):
 
     def on_monitoring_session(self, instance, value):
         self.refresh()
-        self.start_auto_refresh()
+        # self.start_auto_refresh()
 
     def start_auto_refresh(self):
         refresh_interval_seconds = 1
@@ -69,7 +92,7 @@ class SpeciesListLayout(RecycleView):
             Clock.unschedule(self.refresh_clock)
 
         logger.debug(
-            f"Scheduling auto-refresh of species list every {refresh_interval_seconds} seconds"
+            f"Scheduling auto-refresh of species grid every {refresh_interval_seconds} seconds"
         )
         self.refresh_clock = Clock.schedule_interval(
             self.refresh, refresh_interval_seconds
@@ -91,27 +114,30 @@ class SpeciesListLayout(RecycleView):
         expects. In this case the viewclass is a `SpeciesRow` object.
         """
         app = App.get_running_app()
-        species_counts = queries.count_species(app.db_path, ms)
-
-        row_height = 100  # @TODO make dynamic? Or fit images to specific size
+        species = get_unique_species(app.db_path, ms)
+        logger.info(
+            f"Found {len(species)} unique species in monitoring session {ms.id}"
+        )
 
         widget_attrs = [
             {
                 "species": {
                     "name": label or "Unclassified",
                     "count": count,
-                    "image_height": row_height,
-                    "image_path": image_path,
+                    "monitoring_session": ms,
                 },
-                "height": row_height,
+                "height": 300,
+                "width": 1000,
+                "size_hint_y": None,
+                "size_hint_x": 1,
             }
-            for label, count, image_path in species_counts
+            for label, count in species
         ]
 
         return widget_attrs
 
 
-class SpeciesSummaryScreen(Screen):
+class SpeciesSummaryGridScreen(Screen):
     monitoring_session = ObjectProperty()
 
     def on_monitoring_session(self, instance, value):
@@ -119,23 +145,16 @@ class SpeciesSummaryScreen(Screen):
         Update the monitoring session of the child SpeciesListLayout
         when the monitoring session of this screen changes.
         """
-        self.ids.species_list.monitoring_session = self.monitoring_session
+        self.ids.species_grid.monitoring_session = self.monitoring_session
 
     def exit(self):
         self.manager.current = "menu"
 
     def refresh(self):
-        self.ids.species_list.refresh()
+        self.ids.species_grid.refresh()
 
-    def on_enter(self, *args):
-        self.ids.species_list.start_auto_refresh()
+    # def on_enter(self, *args):
+    #     self.ids.species_grid.start_auto_refresh()
 
     def on_leave(self, *args):
-        self.ids.species_list.stop_auto_refresh()
-
-    def export(self):
-        app = App.get_running_app()
-        records = list(get_detected_objects(app.db_path, self.monitoring_session))
-        timestamp = int(time.time())
-        report_name = f"detections-for-{self.monitoring_session.day.strftime('%Y-%m-%d')}-created-{timestamp}"
-        app.export(records, report_name)
+        self.ids.species_grid.stop_auto_refresh()
