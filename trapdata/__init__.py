a8aee5240582b48b66fc0890ddfae3e719065351
Michael Bunsen <notbot@gmail.com>
2023-01-16 18:44:24 -0800

    Remove fix for sqlalchemy-utils, bump version

--- trapdata/__init__.py
+++ trapdata/__init__.py
@@ -1,12 +1,5 @@
 import sys
 import sqlalchemy
-
-# Monkey patch sqlalchmey, remove the "beta" number from the reported version number
-# to allow sqlalchemy_utils to work
-# @TODO Remove once sqlalchemy_utils is updated, or SQLAlchemy 2.0 is out of beta
-# https://github.com/kvesteri/sqlalchemy-utils/pull/644
-sys.modules["sqlalchemy"].__version__ = sqlalchemy.__version__.split("b")[0]
-
 import sentry_sdk
 
 sentry_sdk.init(
