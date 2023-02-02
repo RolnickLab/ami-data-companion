7e3d982daba711493612c57b492b778fb756983c
Michael Bunsen <notbot@gmail.com>
2023-02-02 10:40:01 -0800

    Quick command to open a python shell with DB ready

--- trapdata/cli/base.py
+++ trapdata/cli/base.py
@@ -1,10 +1,11 @@
 import typer
 
-from trapdata.cli import export
+from trapdata.cli import export, shell
 
 
 cli = typer.Typer()
 cli.add_typer(export.cli, name="export")
+cli.add_typer(shell.cli, name="shell")
 
 
 if __name__ == "__main__":
