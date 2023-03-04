4cad8ef4fbc85530966191a8363c45fe47ac76bd
Michael Bunsen <notbot@gmail.com>
2023-03-04 11:23:42 -0800

    Add pipeline test to CLI commands

--- trapdata/cli/test.py
+++ trapdata/cli/test.py
@@ -1,12 +1,20 @@
 import typer
 from rich import print
 
+
+from trapdata.tests import test_pipeline
+
 cli = typer.Typer()
 
 
 @cli.command()
 def nothing():
     print("It works!")
+
+
+@cli.command()
+def pipeline():
+    test_pipeline.run()
 
 
 if __name__ == "__main__":
