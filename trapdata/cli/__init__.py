68cfeea159ea586972984702a4a6522a5e13dbf9
Michael Bunsen <notbot@gmail.com>
2023-03-08 22:13:41 -0800

    Create and migrate in CLI

--- trapdata/cli/__init__.py
+++ trapdata/cli/__init__.py
@@ -1,7 +1,9 @@
 from trapdata.settings import read_settings
+from trapdata.db import check_db
 
 
 settings = read_settings()
+check_db(settings.database_url, create=True, update=True)
 
 
 if __name__ == "__main__":
