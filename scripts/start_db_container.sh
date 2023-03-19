6b058609e36f7d65c10ed8921c7223f306ef4a89
Michael Bunsen <notbot@gmail.com>
2023-03-19 15:55:59 -0700

    Script for starting PostgreSQL db with Docker

---
+++ scripts/start_db_container.sh
@@ -0,0 +1,16 @@
+#! /bin/sh
+
+set -o errexit
+set -o nounset
+
+CONTAINER_NAME=ami-db
+HOST_PORT=5432
+POSTGRES_VERSION=14
+POSTGRES_DB=ami
+
+docker run -d -i --name $CONTAINER_NAME -p $HOST_PORT:5432 -e POSTGRES_HOST_AUTH_METHOD=trust -e POSTGRES_DB=$POSTGRES_DB postgres:$POSTGRES_VERSION
+
+docker logs ami-db --tail 100
+
+echo 'Database started, Connection string: "postgresql://postgres@localhost:5432/ami"'
+echo "Stop (and destroy) database with 'docker stop $CONTAINERNAME'"
