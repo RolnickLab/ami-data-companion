35d2d2a877cee7303faca09a08541e3da942f35b
Michael Bunsen <notbot@gmail.com>
2023-03-21 13:27:26 -0700

    Fix import

--- trapdata/ui/summary.py
+++ trapdata/ui/summary.py
@@ -20,11 +20,8 @@ from trapdata import logger
 from trapdata import constants
 from trapdata.ui.playback import ImagePlaybackScreen
 from trapdata.db.models.events import MonitoringSession
-from trapdata.db.models.detections import (
-    get_detected_objects,
-    export_detected_objects,
-    get_unique_species_by_track,
-)
+from trapdata.db.models.detections import get_detected_objects, export_detected_objects
+from trapdata.db.models.occurrences import get_unique_species_by_track
 
 
 Builder.load_file(str(pathlib.Path(__file__).parent / "summary.kv"))
