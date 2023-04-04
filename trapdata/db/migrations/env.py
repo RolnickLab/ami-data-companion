f3e6f72d0976515c9b848f577aded28c22703376
Michael Bunsen <notbot@gmail.com>
2023-04-04 00:31:44 +0000

    Don't automatically load settings in alembic

--- trapdata/db/migrations/env.py
+++ trapdata/db/migrations/env.py
@@ -3,9 +3,9 @@ from logging.config import fileConfig
 from alembic import context
 from sqlalchemy import engine_from_config, pool
 
-from trapdata.settings import Settings
+# from trapdata.settings import Settings
 
-settings = Settings()
+# settings = Settings()
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -30,7 +30,7 @@ target_metadata = Base.metadata
 # my_important_option = config.get_main_option("my_important_option")
 # ... etc.
 
-config.set_main_option("sqlalchemy.url", str(settings.database_url))
+# config.set_main_option("sqlalchemy.url", str(settings.database_url))
 
 
 def run_migrations_offline() -> None:
