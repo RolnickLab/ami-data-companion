15ffd14ccac37664ab4a153ec0a8cb37f6f4710d
Michael Bunsen <notbot@gmail.com>
2023-04-13 12:39:36 -0700

    Update docker volume for test DB

--- scripts/start_db_container.sh
+++ scripts/start_db_container.sh
@@ -8,7 +8,7 @@ HOST_PORT=5432
 POSTGRES_VERSION=14
 POSTGRES_DB=ami
 
-docker run -d -i --name $CONTAINER_NAME -p $HOST_PORT:5432 -e POSTGRES_HOST_AUTH_METHOD=trust -e POSTGRES_DB=$POSTGRES_DB postgres:$POSTGRES_VERSION
+docker run -d -i --name $CONTAINER_NAME -v "$(pwd)/db_data":/var/lib/postgresql/data --restart always -p $HOST_PORT:5432 -e POSTGRES_HOST_AUTH_METHOD=trust -e POSTGRES_DB=$POSTGRES_DB postgres:$POSTGRES_VERSION
 
 docker logs ami-db --tail 100
 
