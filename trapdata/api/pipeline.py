c8ed0211e512eefd8485e742390ca17eb9559781
Michael Bunsen <notbot@gmail.com>
2023-11-10 20:22:05 -0800

    Separate ML methods for processing images via API

---
+++ trapdata/api/pipeline.py
@@ -0,0 +1,22 @@
+from trapdata import logger
+from trapdata.settings import Settings
+
+from .models import APIMothObjectDetector_FasterRCNN_MobileNet_2023
+
+
+def start_pipeline(
+    source_image_ids: list[int],
+    settings: Settings,
+):
+    logger.info(f"Local user data path: {settings.user_data_path}")
+
+    object_detector = APIMothObjectDetector_FasterRCNN_MobileNet_2023(
+        source_image_ids=source_image_ids,
+        batch_size=settings.localization_batch_size,
+        num_workers=settings.num_workers,
+        user_data_path=settings.user_data_path,
+        single=True,
+    )
+    # if object_detector.queue.queue_count() > 0:
+    object_detector.run()
+    logger.info("Localization complete")
