601cb19b0492a51312d02c2b76c6d907c12b8a98
Michael Bunsen <notbot@gmail.com>
2025-04-28 14:43:04 -0700

    feat: allow setting log level from environment variable

--- trapdata/common/logs.py
+++ trapdata/common/logs.py
@@ -1,24 +1,38 @@
 import logging
+import os
 
 import structlog
 
-# structlog.configure(
-#     wrapper_class=structlog.make_filtering_bound_logger(logging.INFO),
-# )
 
-structlog.configure(
-    wrapper_class=structlog.make_filtering_bound_logger(logging.CRITICAL),
-)
+def get_logger():
+    """
+    Get a logger instance with the specified log level.
 
-logger = structlog.get_logger()
-logging.disable(logging.CRITICAL)
+    Set a log level using the AMI_LOG_LEVEL environment variable. For example:
 
-# import logging
-# from rich.logging import RichHandler
-#
-# FORMAT = "%(message)s"
-# logging.basicConfig(
-#     level="NOTSET", format=FORMAT, datefmt="[%X]", handlers=[RichHandler()]
-# )
-#
-# logger= logging.getLogger("rich")
+    ```
+    export AMI_LOG_LEVEL=DEBUG
+    ami api
+    ```
+
+    or
+
+    ```
+    AMI_LOG_LEVEL=CRITICAL ami api
+    ```
+
+    @TODO
+    It would be ideal if we could configure a log level in the Settings class,
+    but there are issues with circular imports.
+    """
+    log_level = logging.getLevelName(os.environ.get("AMI_LOG_LEVEL", "INFO"))
+
+    structlog.configure(
+        wrapper_class=structlog.make_filtering_bound_logger(log_level),
+    )
+
+    logger = structlog.get_logger()
+    return logger
+
+
+logger = get_logger()
